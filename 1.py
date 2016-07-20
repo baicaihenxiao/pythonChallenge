0ef7df05e052628198ad7311868f946b1693acdc
baicaihenxiao <baicaihenxiao@gmail.com>
2016-07-20 13:21:36 +0800

    level 1/2016-7-20 13:21:33

---
+++ 1.py
@@ -0,0 +1,23 @@
+#2016-7-20 13:20:12
+#http://www.pythonchallenge.com/pc/def/map.html
+
+
+from __future__ import print_function
+text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+for c in text:
+	if c.isalpha():
+		print(chr((ord(c) - ord('a') + 2)%26 + ord('a')), end='')
+	else:
+		print(c, end='')
+		
+url =  "http://www.pythonchallenge.com/pc/def/map.html"
+
+
+
+for c in url:
+	if c.isalpha():
+		print(chr((ord(c) - ord('a') + 2)%26 + ord('a')), end='')
+	else:
+		print(c, end='')
+		
+#the result is to replace map with ocr in the URL.
