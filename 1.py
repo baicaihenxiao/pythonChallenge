d8041624de101c2cb459a95c8d2a657bfe705183
baicaihenxiao <baicaihenxiao@gmail.com>
2016-07-23 00:18:03 +0800

    level 1 update/2016-7-23 00:17:59

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@
 #http://www.pythonchallenge.com/pc/def/map.html
 
 
-from __future__ import print_function
+from __future__ import print_function# this line is to use print(c, end = ''), which doesn't output new line;
 text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 for c in text:
 	if c.isalpha():
@@ -16,7 +16,7 @@ url =  "http://www.pythonchallenge.com/pc/def/map.html"
 
 for c in url:
 	if c.isalpha():
-		print(chr((ord(c) - ord('a') + 2)%26 + ord('a')), end='')
+		print(chr((ord(c) - ord('a') + 2)%26 + ord('a')), end='')#ord('a') output the ascII of a;
 	else:
 		print(c, end='')
 		
