9ee33975a335355afc35815a713fdcbaae9594ae
baicaihenxiao <baicaihenxiao@gmail.com>
2016-08-02 20:56:36 +0800

    test update/2016-8-2 20:56:33

--- test/ScipyTest.py
+++ test/ScipyTest.py
@@ -1,10 +1,17 @@
-#import scipy
-#scipy.misc
-import scipy.misc
-
-f = scipy.misc.face()
-scipy.misc.imsave('face.png', f)
-
+from scipy import misc
 import matplotlib.pyplot as plt
+'''f = misc.face()
+misc.imsave('face.png', f) # uses the Image module (PIL)
 plt.imshow(f)
+plt.show()'''
+face = misc.imread('face.png')
+print type(face)
+print face.shape, face.dtype
+face.tofile('face.raw')
+face_from_raw = np.fromfile('face.raw', dtype=np.uint8)
+'''
+from PIL import Image
+im = Image.open('face.png')
+plt.imshow(im)
 plt.show()
+'''
