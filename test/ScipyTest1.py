ec827527201fbb4128cd923be76a6c332dbfe8e1
baicaihenxiao <baicaihenxiao@gmail.com>
2016-08-08 19:33:06 +0800

    image process test/2016-8-8 19:33:01

--- test/ScipyTest1.py
+++ test/ScipyTest1.py
@@ -1,23 +1,35 @@
 from PIL import Image
-import matplotlib.pyplot as plt
-import pylab
+import os, sys
+
+print sys.argv[0:]
+
+im = Image.open("dna.jpeg")
+print im.format, im.size, im.mode
+#im.show()
+#print "%dx%d" % im.size, im.mode
+
+print im.split()
 
 '''
-im = misc.imread('face.png')
-print type(im)
-print im.shape, im.dtype
-print im.max(), im.min()
+box = (100, 100, 400, 400)
+region = im.crop(box)
+region = region.transpose(Image.ROTATE_180)
+im.paste(region, box)
+
+im.show()
 '''
-#dna = Image.open('dna.jpeg')
-dna = misc.imread('dna.jpeg')
-print type(dna)
-print dna.shape, dna.dtype
-print dna.max(), dna.min()
+
+im1 = Image.open("face.png")
+print im1.format, im1.size, im1.mode
+im1.show()
+print im1.split()
+r, g, b = im1.split()
+print r, g, b
+im1 = Image.merge("RGB", (b, g, r))
+im1.show()
 
 
-T = misc.thresholding.otsu(dna)
-pylab.imshow(dna > T)
-pylab.show()
+
 
 
 
